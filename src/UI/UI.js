32a848425c2b3a88ea9ea3463bc213afed45793a
Trevor <tsesurface@gmail.com>
2019-02-23 11:29:39 -0800

    Buttons

---
+++ src/UI/UI.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'; 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
+class UI extends Component { 
+    return ( 
+        <Container className="fluid">
+            <Row>
+                <Col xs="2"> Ingredients </Col>
+                <Col xs="2"> Blank </Col>
+                <Col xs="2"> Cam </Col>
+                <Col xs="2"> Blank </Col>
+                <Col xs="2"> Get Recipes </Col>
+            </Row>
+        </Container>
+    ); 
+} 
+export default UI;
