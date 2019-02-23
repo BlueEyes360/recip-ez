17550f25a9295028dd9a6770e809edf89df17040
Trevor <tsesurface@gmail.com>
2019-02-23 14:53:02 -0800

    Added button Graphic

--- src/UI/UI.js
+++ src/UI/UI.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react'; 
+import React, {Component} from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+
+import ingredientImage from './assets/ingredientPic.png';
+import cameraImage from './assets/Camera.png';
+import recipeImage from './assets/Recipes.png';
 
 class UI extends Component {
-
     render() {
-        return (
-
+        return(
             <Container className="fluid">
-                <Row>
-                    <Col xs="2"> Ingredients </Col>
-                    <Col xs="2"> Blank </Col>
-                    <Col xs="2"> Cam </Col>
-                    <Col xs="2"> Blank </Col>
-                    <Col xs="2"> Get Recipes </Col>
+               <Row>     
+                    <Col xs={4}>
+                        <Button variant="outline-secondary"> 
+                            <p className="font-weight-bolder"> INGREDIENTS </p>                            
+                            <Image src={ingredientImage} rounded />
+                        </Button>
+                    </Col> 
+                    <Col xs={4}> 
+                        <Button variant="outine-secondary">
+                            <Image src={cameraImage} rounded />
+                        </Button>
+                    </Col>
+                    <Col xs={4}>
+                        <Button variant="outline-secondary">
+                            <p className="font-weight-bolder"> RECIPES </p>                            
+                            <Image src={recipeImage} rounded />
+                        </Button>
+                    </Col>
                 </Row>
             </Container>
         );
     }
-
 }
 export default UI;
