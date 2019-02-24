2834b8797295273857d201ca82af7f769d33dac1
Mike Roper <micheal.roper@wsu.edu>
2019-02-23 20:53:58 -0800

    Not needed functions taken out.
    took out a functions for checking if card is true or false.  Not needed as as recipes are linked to outside website

--- src/RecipeUI/RecipUI.js
+++ src/RecipeUI/RecipUI.js
@@ -3,18 +3,11 @@ import Card from 'react-bootstrap/Card';
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-//import DisplayRecip from './DisplayRecipe/DisplayRecip.js';
 
 class displayScreen extends Component {
     state = {
-        // card: true,
         homeCard: true,
     }
-    
-    // clickHandler = () => {
-    //     let oldCardValue = this.state.card;
-    //     this.setState({card: !oldCardValue});
-    // }
 
     clickHome = () => {
         let oldHomeValue = this.state.homeCard;
@@ -30,7 +23,8 @@ class displayScreen extends Component {
                     <Card.Body>
                         <Card.Title>{this.props.title}</Card.Title>
                         <Card.Text>
-                            {this.props.ingredients}
+                            Ingredients for Recipe: 
+                            <p>{this.props.ingredients}</p>
                         </Card.Text>
                         
                         <Button onClick={() => this.clickHome()}>Home</Button>
@@ -40,21 +34,6 @@ class displayScreen extends Component {
                 </Card>
             </div>
         )
-
-        // if(this.state.card === false) {
-        //     display = (
-        //         <Jumbotron>
-        //             <h1>{this.props.title}</h1>
-        //             <p>
-        //                 This is a simple hero unit, a simple jumbotron-style component for calling
-        //                 extra attention to featured content or information.
-        //             </p>
-        //         </Jumbotron>
-
-        //     )
-        // }
-
-
 
         if(this.state.homeCard === false) {
             //this should go back to home screen
@@ -81,6 +60,3 @@ class displayScreen extends Component {
 
 export default displayScreen;
 
-// variant="top" className = "img-thumbnail"
-//href="#"
-//onClick={() => this.clickHandler()}
